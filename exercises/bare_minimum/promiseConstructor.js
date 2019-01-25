47a6731fb75f47bef823a2c5dd33ba748f88d291
elizabethcan <elizabeth230@gmail.com>
2019-01-24 16:19:11 -0800

    Implement promisification on existing functions

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,15 +7,33 @@
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
+// Promise.promisifyAll(fs);
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, (err, fileData) => {
+      if (err) {
+        reject(err);
+      } else {
+        var lines = fileData.toString().split('\n');
+        resolve(lines[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    request(url, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
